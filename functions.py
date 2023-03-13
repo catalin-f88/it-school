fc405b26b81d1cf856b2925990e9284ec07dba78
Catalin F <catalin.nemesis@gmail.com>
2023-03-13 21:04:25 +0200

    Curs

--- functions.py
+++ functions.py
@@ -1,6 +1,59 @@
-def greet(name):
-    print("*" * 30)
-    print(f"Hello, {name}!")
-    print("*" * 30)
+# def greet(name):
+#     """Say formatted message to user."""
+#     print("*" * 30)
+#     print(f"Hello, {name}!")
+#     print("*" * 30)
 
-greet("Catalin")
+# greet("Catalin")
+
+# x = int(input("Introdu numar: "))
+
+# def verify_parity(x):
+#     if x % 2 == 0:
+#         return True
+#     else:
+#         return False
+
+# for i in range(0, 51):
+#     if not verify_parity(i):
+#         print(i)
+
+
+# def range_product(start, stop):
+#     """Calculate the product for numbers between start and stop."""
+#     prod = 1
+#     for i in range(start, stop):
+#         prod *= i
+#     return prod
+
+# start_in = int(input("Introdu numar: "))
+# stop_in = int(input("Introdu numar: "))
+
+# print(f"Produsul numerelor din intervalul {start_in} si {stop_in} este {range_product(start_in, stop_in)}")
+
+
+
+inaltime = float(input("Introdu un numar (inaltime): "))
+latime = float(input("Introdu un numar (latime): "))
+lungime = float(input("Introdu un numar (lungime): "))
+grosime = float(input("Introdu un numar (grosime): "))
+
+def pret_cutii(inaltime, latime, lungime, grosime):
+    # if not isinstance(latime, float):
+    #     return 0
+    # if not isinstance(inaltime, float):
+    #     return 0
+    # if not isinstance(lungime, float):
+    #     return 0
+    # if not isinstance(grosime, float):
+    #     return 1
+    
+    pret = inaltime * latime * lungime * 25
+    if grosime == 2:
+        return pret * 1.10
+    elif grosime == 3:
+        return pret * 1.20
+    return pret
+
+print(f"Pretul unei cutii cu inaltime {inaltime}, latime {latime}, lungime {lungime} si grosime {grosime} este {pret_cutii(inaltime,latime,lungime,grosime):.2f} lei.")
+
---
+++ homework5.py
@@ -0,0 +1,49 @@
+# full_name = {
+#     "first_name": "Catalin",
+#     "last_name:": "Florea"
+# }
+
+# print("My full name is {} {}".format(full_name["first_name"], full_name["last_name:"]))
+
+# name = input("Insert your name here: ")
+# age = input("What is your age? ")
+
+# print(f"Hello, {name}! You are {age} years old!")
+
+# shopping_list = ["mere", "pere", "alune", "cirese"]
+# print("Astazi trebuie sa cumperi urmatoarele: {}.".format(", ".join(shopping_list)))
+
+# print("{:^9}".format("$"))
+# for i in range(3, 10, 2):
+#     v = "*" * i
+#     print(f"{v:^9}")
+
+# lorem = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+# product_code_list = [
+#     "mmf2",
+#     "xdfr",
+#     "ef3r",
+#     "efc2",
+#     "sddf",
+#     "weee"
+# ]
+
+# import_numbers = "Today we had 280 clients."
+
+# commit = """commit 30c06bce50eeb7a8856e18df2dc64e76fec14cc9
+# Author: Dinu Mihai <mihai.dinu93@gmail.com>
+# Date:   Thu Jun 9 18:18:02 2022 +0300
+
+#     shuffle method"""
+
+# py_text = """Python is an interpreted, interactive, object-oriented programming language. It incorporates modules, exceptions, dynamic typing, very high level dynamic data types, and classes. It supports multiple programming paradigms beyond object-oriented programming, such as procedural and functional programming. Python combines remarkable power with very clear syntax. It has interfaces to many system calls and libraries, as well as to various window systems, and is extensible in C or C++. It is also usable as an extension language for applications that need a programmable interface. Finally, Python is portable: it runs on many Unix variants including Linux and macOS, and on Windows.
+# To find out more, start with The Python Tutorial. The Beginner's Guide to Python links to other introductory tutorials and resources for learning Python."""
+
+# print(lorem[:10])
+# print(lorem[-10:])
+
+# for i, v in enumerate(product_code_list):
+#     product_code_list[i] = f"{v[:-1]}x"
+
+# print(product_code_list)
+
